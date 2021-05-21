7af3cfaeea05a877cdf137ca64d2fe8cdb0670a4
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2021-05-21 19:31:33 +0300

    test

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,10 @@ var ccard = document.getElementById("card");
 var ctitle = document.getElementById("title");
 var ctext = document.getElementById("text");
 var cimg = document.getElementById("img");
-alert('linked');
 
 ccard.style.display = "none";
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
-
-
-
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             posts = JSON.parse(this.response);
@@ -23,18 +19,14 @@ function loadDoc() {
 }
 
 function arrangeDocs(params) {
-    alert('step 2');
     params.forEach(post => {
         var pd = post.time;
         if (post.show == 'off') {
-            document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "<div class='card'>  <div class='card-body'><div class='card-content'><div class='mb-1 text-muted'>" + pd + "</div> <h5 class='card-title'>" + post.title + "</h5><p class='card-text'>" + post.content + "</p></div></div></div>";
-            alert('on');
+            document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "<div class='card'>  <div class='card-body'><div class='card-content'><div class='mb-1 text-muted'>" + pd + "</div> <h5 class='card-title'>" + post.title + "</h5><p class='card-text'>" + post.content + "</p></div></div></div>"
 
         }
         else if (post.show == 'on') {
-            document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "<div class='card'> <img src='https://main-pwep-ldhnayhlcqhu1y2u-gtw.qovery.io/posts/uploads/" + post.img + "' alt='Inspire' class='img-fluid'> <div class='card-body'><div class='card-content'><div class='mb-1 text-muted'>" + pd + "</div> <h5 class='card-title'>" + post.title + "</h5><p class='card-text'>" + post.content + "</p></div></div></div>";
-            
-            alert('off);
+            document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "<div class='card'> <img src='https://main-pwep-ldhnayhlcqhu1y2u-gtw.qovery.io/posts/uploads/" + post.img + "' alt='Inspire' class='img-fluid'> <div class='card-body'><div class='card-content'><div class='mb-1 text-muted'>" + pd + "</div> <h5 class='card-title'>" + post.title + "</h5><p class='card-text'>" + post.content + "</p></div></div></div>"
 
         }
     });
