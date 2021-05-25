6189b9ddee7df89da1d30534794c504ca199f860
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2021-05-25 14:28:57 +0300

    a r

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 function loadDoc() {
-    alert("called")
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
